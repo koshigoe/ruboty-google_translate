b052a6fe530085c7c053f5413403fff9b49ac9d5
koshigoe <koshigoeb@gmail.com>
2017-01-27 21:48:05 +0900

    Define required environment variables.

--- lib/ruboty/handlers/google_translate.rb
+++ lib/ruboty/handlers/google_translate.rb
@@ -1,6 +1,8 @@
 module Ruboty
   module Handlers
     class GoogleTranslate < Base
+      env :GOOGLE_CLOUD_PROJECT, 'Google Cloud Project ID'
+      env :GOOGLE_CLOUD_KEY, 'Google Cloud API Key'
     end
   end
 end
